7d981a718e70ca96653d15654f490950c041fe78
RASHMIRANJAN NAYAK <141851759+rashmi0852@users.noreply.github.com>
2023-09-07 12:46:45 +0530

    branch

---
+++ explore.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 13 22:43:42 2022
+
+@author: rashmi
+"""
+
+import streamlit as st
+
+
+
+
+def show_explore():
+    st.title("Explore to the data set")
+    
+show_explore()
